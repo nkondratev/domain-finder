da910b63026be31a75852ea166e16ee3f4e27894
nikita <nikita.kondratiev01@gmail.com>
2023-04-03 12:53:11 +0300

    Добавили треды

--- start.py
+++ start.py
@@ -1,18 +1,37 @@
 #!/bin/python3
+
+import threading
 import dns.resolver
 import whois
+import random
+import string
+import time
 
-with open('domains1.txt') as domains:
-    for domain in domains:
-        domain = domain.strip() + ".ru"
+zone = ".ru"
+
+def part_domain(fzone):
+    domain = ''
+    for i in range(3):
+        domain += random.choice(string.ascii_lowercase + string.digits)
+    domain += fzone
+    return domain
+
+def main_domain():
+        domain_name = part_domain(zone)
         try:
-            result = dns.resolver.resolve(domain)
+            result = dns.resolver.resolve(domain_name)
         except KeyboardInterrupt:
-            break
+            quit()
         except:
             try:
-                result = whois.whois(domain)
+                result = whois.whois(domain_name)
             except:
-                print("Domain found", domain)
+                print("Domain found", domain_name)
 
-        
+while True:
+    for i in range(5):
+        my_thread = threading.Thread(
+            target=main_domain, args=())
+        my_thread.start()
+    for i in range(5):
+        my_thread.join()
